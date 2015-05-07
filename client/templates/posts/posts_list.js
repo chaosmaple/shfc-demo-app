c27aa53774c97626870cbb8972f677ab36f2cff7
mayo <chaosmaple@gmail.com>
2015-05-08 01:59:45 +0800

    yees, this is the first commit :smirk:

---
+++ client/templates/posts/posts_list.js
@@ -0,0 +1,22 @@
+var postsData = [
+    {
+        title: 'fuck that game',
+        postUser: 'man',
+        disQuan: 22,
+        lastRe: 'pafee',
+    },{
+        title: 'yes we know',
+        postUser: 'mayo',
+        disQuan: 13,
+        lastRe: 'ushachi',
+    },{
+        title: 'i dont know what you say',
+        postUser: 'hahaha',
+        disQuan: 754,
+        lastRe: 'gagaga',
+    }
+]
+
+Template.postsList.helpers({
+    posts: postsData
+})
